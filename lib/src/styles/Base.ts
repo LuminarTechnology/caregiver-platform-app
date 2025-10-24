c9e424598d0f23c5d91d3c211c7b046578feef88
mahian <mahianahmad9@gmail.com>
2025-10-24 23:11:37 +0600

    added basic style variable like color fontsize and fontwidtch

--- lib/src/styles/Base.ts
+++ lib/src/styles/Base.ts
@@ -1,5 +1,22 @@
 export const BaseStyle = {
   color: {
-    primary: '#A41845'
+    primary: '#A41845',
+    secondary: '#FFF7F9',
+    muted: '#C3C4BF',
+    foreground: '#FFF7F9',
+    background: '#ffffff'
+  },
+  fontSize: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 20,
+    xl: 24
+  },
+  fontWeight: {
+    normal: '400',
+    medium: '500',
+    semibold: '600',
+    bold: '700'
   }
 } as const
