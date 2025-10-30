1f8e7201e03bfc2efebfedd2a6a33b9af92d19ac
mahian <mahianahmad9@gmail.com>
2025-10-31 01:24:09 +0600

    add signin and signup page and create a reusable input field and add validation for all input field

---
+++ app/client/src/navigation/AuthStack.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import SignIn from "../screens/auth/SignIn";
+import SignUp from "../screens/auth/SignUp";
+
+export type AuthStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
+};
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
+
+export default function AuthStack() {
+  return (
+    <Stack.Navigator id={undefined} screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+}
--- app/client/src/navigation/RootStack.tsx
+++ app/client/src/navigation/RootStack.tsx
@@ -1,9 +1,11 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import BottomTabs from './BottomTabs'
+import AuthStack from './AuthStack'
 
 export type RootStackParamList = {
   Main: undefined
+  AuthStack: undefined
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
@@ -12,10 +14,11 @@ const RootStack = () => {
   return (
     <Stack.Navigator
       id={undefined}
-      initialRouteName="Main"
+      initialRouteName="AuthStack"
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="Main" component={BottomTabs} />
+      <Stack.Screen name="AuthStack" component={AuthStack} />
     </Stack.Navigator>
   )
 }
