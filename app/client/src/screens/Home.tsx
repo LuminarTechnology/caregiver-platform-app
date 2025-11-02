d06a6ad806c7adc6a9b416184ec4e584b570c437
Mh-Monzil <monzil246@gmail.com>
2025-11-02 15:15:55 +0600

    refactor: restore HomeScreen layout and remove GetStarted component

--- app/client/src/screens/Home.tsx
+++ app/client/src/screens/Home.tsx
@@ -4,15 +4,14 @@ import GetStarted from './auth/GetStarted'
 
 const HomeScreen = () => {
   return (
-    <GetStarted />
-    // <View className="bg-primary flex h-screen items-center justify-center gap-6">
-    //   <View className="bg-foreground rounded-full p-6">
-    //     <HomeFilledIcon height={36} width={36} />
-    //   </View>
-    //   <Text className="text-foreground text-center text-3xl font-semibold">
-    //     Caregiver Client
-    //   </Text>
-    // </View>
+    <View className="bg-primary flex h-screen items-center justify-center gap-6">
+      <View className="bg-foreground rounded-full p-6">
+        <HomeFilledIcon height={36} width={36} />
+      </View>
+      <Text className="text-foreground text-center text-3xl font-semibold">
+        Caregiver Client
+      </Text>
+    </View>
   )
 }
 
