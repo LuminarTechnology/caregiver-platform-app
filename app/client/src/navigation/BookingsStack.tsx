4bb40dbaeb373c949bea86626ddd6f8700b6171d
amitsurjo <amitgoldersurjo@gmail.com>
2025-11-23 00:54:14 +0600

    navigation

--- app/client/src/navigation/BookingsStack.tsx
+++ app/client/src/navigation/BookingsStack.tsx
@@ -1,9 +1,19 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import MyBookingsScreen from '../screens/MyBookings'
+import ChildCareScreen from '../screens/BookCaregiverChild/ChildCareScreen'
+import ChildCareScreen2 from '../screens/BookCaregiverChild/ChildCareScreen2'
+import ChildCareScreen3 from '../screens/BookCaregiverChild/ChildCareScreen3'
+import ChildCareScreen4 from '../screens/BookCaregiverChild/ChildCareScreen4'
+import ChildCareScreen5 from '../screens/BookCaregiverChild/ChildCareScreen5'
 
 export type BookingsStackParamList = {
   BookingsMain: undefined
+  ChildCare: undefined
+  ChildCare2: undefined
+  ChildCare3: undefined
+  ChildCare4: undefined
+  ChildCare5: undefined
   // Add booking detail screens here if needed
 }
 
@@ -13,6 +23,11 @@ export default function BookingsStack() {
   return (
     <Stack.Navigator id={undefined} screenOptions={{ headerShown: false }}>
       <Stack.Screen name="BookingsMain" component={MyBookingsScreen} />
+      <Stack.Screen name="ChildCare" component={ChildCareScreen} />
+      <Stack.Screen name="ChildCare2" component={ChildCareScreen2} />
+      <Stack.Screen name="ChildCare3" component={ChildCareScreen3} />
+      <Stack.Screen name="ChildCare4" component={ChildCareScreen4} />
+      <Stack.Screen name="ChildCare5" component={ChildCareScreen5} />
     </Stack.Navigator>
   )
 }
